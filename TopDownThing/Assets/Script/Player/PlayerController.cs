fb1e2818e22efa2e7e1840117e4b55069d4b2da3
641310523 <156014191+FCW-1@users.noreply.github.com>
2024-02-09 15:31:18 +0700

    add camera and playercontrol

---
+++ TopDownThing/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 input:
+
+    void Gather Input()
+    {
+        input = new Vector3(Input.GetAxisRaw())
+    }
+}
