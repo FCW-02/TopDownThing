59533178946d8438d04b4e96ff0dd0ddc8a372bb
641310523 <156014191+FCW-1@users.noreply.github.com>
2024-02-26 19:16:04 +0700

    back to no jump / change text color

--- TopDownThing/Assets/Script/Player/PlayerController.cs
+++ TopDownThing/Assets/Script/Player/PlayerController.cs
@@ -6,19 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed = 5;
-    [SerializeField] private float turnSpeed = 360;
-    [SerializeField] private float jumpForce = 10f; // New variable for jump force
+    [SerializeField] private float turnspeed = 360;
     private Vector3 input;
 
     void Update()
     {
         GatherInput();
         Look();
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            Jump(); // Call Jump() method when jump input is detected
-        }
     }
 
     void FixedUpdate()
@@ -42,17 +36,12 @@ public class PlayerController : MonoBehaviour
             var relative = (transform.position + skewedInput) - transform.position;
             var rot = Quaternion.LookRotation(relative, Vector3.up);
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnspeed * Time.deltaTime);
         }
     }
 
     void Move()
     {
         rb.MovePosition(transform.position + (transform.forward * input.magnitude) * speed * Time.deltaTime);
-    }
-
-    void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
