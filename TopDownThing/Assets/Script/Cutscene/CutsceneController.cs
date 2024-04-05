40c82cd8e73166f13fc2f931a2a849589af7e7bd
641310523 <156014191+FCW-1@users.noreply.github.com>
2024-04-06 05:33:24 +0700

    disable ui when cutscene

--- TopDownThing/Assets/Script/Cutscene/CutsceneController.cs
+++ TopDownThing/Assets/Script/Cutscene/CutsceneController.cs
@@ -6,53 +6,56 @@ public class CutsceneController : MonoBehaviour
 {
     public Camera mainCamera;
     public Camera cutsceneCamera;
-    public GameObject player; // Reference to the player GameObject
-    public float cutsceneDuration = 6f; // Duration of the cutscene in seconds
+    public GameObject player;
+    public Canvas uiCanvas; // Reference to the UI Canvas
+    public float cutsceneDuration = 6f;
 
-    private PlayerController playerController; // Reference to the PlayerController script
+    private PlayerController playerController;
     private bool cutsceneActive = false;
     private float cutsceneTimer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Disable the main camera at the beginning
         mainCamera.enabled = false;
-        // Enable the cutscene camera
         cutsceneCamera.enabled = true;
         cutsceneActive = true;
 
-        // Get the PlayerController component from the player GameObject
         playerController = player.GetComponent<PlayerController>();
 
-        // Disable player movement during the cutscene
         if (playerController != null)
         {
             playerController.enabled = false;
+        }
+
+        if (uiCanvas != null)
+        {
+            uiCanvas.enabled = false;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check if the cutscene is active
         if (cutsceneActive)
         {
-            // Increment the timer
             cutsceneTimer += Time.deltaTime;
 
-            // Check if the cutscene duration has elapsed
             if (cutsceneTimer >= cutsceneDuration)
             {
-                // Switch back to the main camera
+
                 mainCamera.enabled = true;
                 cutsceneCamera.enabled = false;
                 cutsceneActive = false;
 
-                // Re-enable player movement after the cutscene
                 if (playerController != null)
                 {
                     playerController.enabled = true;
+                }
+
+                if (uiCanvas != null)
+                {
+                    uiCanvas.enabled = true;
                 }
             }
         }
--- TopDownThing/Assets/Script/Cutscene/CutsceneManage.cs
+++ TopDownThing/Assets/Script/Cutscene/CutsceneManage.cs
@@ -6,54 +6,47 @@ using UnityEngine.UI;
 
 public class CutsceneManage : MonoBehaviour
 {
-    public Canvas cutsceneCanvas; // Reference to the Canvas containing your cutscene
-    public float imageDuration = 2f; // Duration each image should be displayed in seconds
-    public string levelToLoad = "YourLevelScene"; // Name of the level scene to load after the cutscene
+    public Canvas cutsceneCanvas;
+    public float imageDuration = 2f;
+    public string levelToLoad = "YourLevelScene";
 
-    private Image[] images; // Array to hold references to all images in the cutscene
-    private int currentImageIndex = 0; // Index of the current image being displayed
-    private bool isCutsceneFinished = false; // Flag to track whether the cutscene has finished
+    private Image[] images;
+    private int currentImageIndex = 0;
+    private bool isCutsceneFinished = false;
 
     private void Start()
     {
-        // Get all Image components under the cutscene Canvas
+
         images = cutsceneCanvas.GetComponentsInChildren<Image>();
 
-        // Start displaying images sequentially
         DisplayNextImage();
     }
 
     private void DisplayNextImage()
     {
-        // Disable all images
+
         foreach (Image image in images)
         {
             image.gameObject.SetActive(false);
         }
 
-        // Enable the next image in the sequence
         images[currentImageIndex].gameObject.SetActive(true);
 
-        // Move to the next image index
         currentImageIndex++;
 
-        // If all images have been displayed, indicate that the cutscene has finished
         if (currentImageIndex >= images.Length)
         {
             isCutsceneFinished = true;
             return;
         }
 
-        // Schedule the next image to be displayed after the specified duration
         Invoke("DisplayNextImage", imageDuration);
     }
 
     private void Update()
     {
-        // Check if the cutscene has finished
         if (isCutsceneFinished)
         {
-            // Load the level scene
             SceneManager.LoadScene(levelToLoad);
         }
     }
